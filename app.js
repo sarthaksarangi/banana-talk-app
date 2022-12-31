4848dab159df4912a6ed00b18dbde0ac184c6949
Sarthak Sarangi <77694787+sarthaksarangi@users.noreply.github.com>
2022-12-31 23:39:20 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var btnTranslate= document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input');
 console.log(txtInput);
+var outputDiv = document.querySelector("#output")
+outputDiv.innerText = 'Hello'
 
 
 
 btnTranslate.addEventListener('click',function clicked(){
         console.log("clicked");
+
     });
 
