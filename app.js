dc3124a85143886e7ee9e06a55211c69cc74dbfa
Sarthak Sarangi <77694787+sarthaksarangi@users.noreply.github.com>
2022-12-31 14:22:34 +0530

    edited

---
+++ app.js
@@ -0,0 +1,10 @@
+var btnTranslate= document.querySelector('#btn-tanslate');
+var txtInput = document.querySelector('#txt-input');
+
+
+
+if(btnTranslate){
+    btnTranslate.addEventListener('click',function clicked(){
+        console.log("clicked");
+    });
+}
