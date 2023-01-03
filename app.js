b3520aba0f21b06f598514f803f985f090229a09
Sarthak Sarangi <77694787+sarthaksarangi@users.noreply.github.com>
2023-01-03 19:46:50 +0530

    finished

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
-var btnTranslate= document.querySelector('#btn-translate');
-var txtInput = document.querySelector('#txt-input');
-var outputDiv = document.querySelector("#output")
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var translateButton = document.querySelector("#translate-button");
 
-function translationURL(text){
- return serverURL+"?text="+text;
+translateButton.addEventListener("click", buttonClickHandler)
+
+
+var translateInput = document.querySelector("#translate-input");
+
+
+var translateOutput = document.querySelector("#translate-output");
+
+// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+// REPL URL: https://repl.it/@tanaypratap/lessonfourapi
+
+function buttonClickHandler(event) {
+    console.log("button clicked");
+    var input = translateInput.value;
+    var finalURL = constructURL(input);
+    console.log(finalURL);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            translateOutput.innerText = json.contents.translated;
+        })
+        .catch(() => alert("some error occured"))
+    
 }
-function errorHandler(error){
- console.log('Error Occured')
- alert('Something is wrong with the server')
+
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${url}?text=${encodedURI}`;
 }
-
-function clickHandler(){
-    var inputTEXT = txtInput.value;
-    fetch(translationURL(inputTEXT))
-         .then(response=> response.json)
-         .then(json=> {
-        var translatedText = json.contents.translated;
-        outputDiv.innerText = translatedText;
-        
-    })
-    .catch(errorHandler)
-}
-
-btnTranslate.addEventListener("click",clickHandler());
-
